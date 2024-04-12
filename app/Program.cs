53d5ea39a6c18183ae0f2d5dc3a4841b29cce6e8
s24762 <s24762@pjwstk.edu.pl>
2024-04-12 22:31:39 +0200

    change variable name v2

--- app/Program.cs
+++ app/Program.cs
@@ -3,9 +3,9 @@
         throw new ArgumentException("Array cannot be null or empty.");
 
     int max = numbers[0];
-    for (int i = 1; i < numbers.Length; i++) {
-        if (numbers[i] > max)
-            max = numbers[i];
+    for (int inna_zmienna_v2 = 1; inna_zmienna_v2 < numbers.Length; inna_zmienna_v2++) {
+        if (numbers[inna_zmienna_v2] > max)
+            max = numbers[inna_zmienna_v2];
     }
     return max;
 }
